b5c3de32c081f8194ada5e8a629e9d66b2c58fab
AKSAR <akshatjaiswal2518@gmail.com>
2024-05-26 13:56:41 +0530

    database connection made

---
+++ src/db/connect.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const DB = process.env.MONGO_URI;
+
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log(`Connection Successful`);
+    })
+    .catch((err) => console.log(`No Connection`));
