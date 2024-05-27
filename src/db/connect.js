ab73d3244669e6fed06f104075b1b249692e5be5
AKSAR <akshatjaiswal2518@gmail.com>
2024-05-27 14:04:22 +0530

    add utils code

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { app } = require("../app");
 
 const DB = process.env.MONGO_URI;
 
@@ -6,5 +7,8 @@ mongoose
     .connect(DB)
     .then(() => {
         console.log(`Connection Successful`);
+        app.listen(process.env.PORT || 3000, () =>
+            console.log(`Server is running on port 3000`)
+        );
     })
     .catch((err) => console.log(`No Connection`));
